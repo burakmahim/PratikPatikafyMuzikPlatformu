e6447c11ec2c10e6277252a21fab506f0d846d80
burakmahim <burakmahim718@gmail.com>
2024-11-27 11:27:17 +0300

    Proje dosyası ekle.

---
+++ PratikPatikafyMuzikPlatformu/Singer.cs
@@ -0,0 +1,10 @@
+﻿namespace PratikPatikafyMuzikPlatformu
+{
+    public class Singer
+    {
+        public string SingerNameSurname { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public int AlbumSales { get; set; }
+    }
+}
